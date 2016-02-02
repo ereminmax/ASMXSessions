905df2e212137fce403d292e8090e144f05660d5
Maxim Eremin <eremin.max@gmail.com>
2016-02-02 18:13:43 +0300

    Final commit

---
+++ ProteinTrackerWebServices/User.cs
@@ -0,0 +1,10 @@
+namespace ProteinTrackerWebServices
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Goal { get; set; }
+        public int Total { get; set; }
+        public int UserId { get; set; }
+    }
+}
